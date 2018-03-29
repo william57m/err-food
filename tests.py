d55376f0c25a7f85de53fc39c4220136dfc2d681
William Mura <william57m@gmail.com>
2018-03-28 22:14:34 -0400

    Refactoring + add search command

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 """Basic output validation tests"""
 
+import re
 import food
 
 # pytest
@@ -11,13 +12,20 @@ def test_resto_pick(testbot):
     testbot.push_message('!resto pick')
     result = testbot.pop_message()
     assert result is not None
+    restaurant = re.sub('I suggest ', '', result)
+    assert restaurant in food.RESTAURANT_LIST
 
 def test_resto_yelp(testbot):
     testbot.push_message('!resto yelp')
     result = testbot.pop_message()
     assert result is not None
 
+def test_resto_search(testbot):
+    testbot.push_message('!resto search burger')
+    result = testbot.pop_message()
+    assert result is not None
+
 def test_resto_doc(testbot):
     testbot.push_message('!resto unvalid_command')
     result = testbot.pop_message()
-    assert result == "This is not a valid command man, please use 'pick' or 'yelp'"
+    assert result == "This is not a valid command man, please use 'pick', 'search <text>' or 'yelp'"
